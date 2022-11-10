aacf7b89caacc84f1d445531aa32a46540e78de2
Kasen <18170166+imkasen@users.noreply.github.com>
2022-11-10 11:21:54 +0800

    turn webp to png

--- README.md
+++ README.md
@@ -1,26 +1,41 @@
 # WebP Converter
 
-A simple conversion script that can bulk convert images to WebP.
+A simple conversion script that can bulk convert images to WebP or vice versa.
 
-## Install WebP
+## Install Dependencies
 
 * macOS: `brew install webp`
-* Debian/Ubuntu: `apt install webp`
-* [Manual download](https://developers.google.com/speed/webp/download)
+* Debian / Ubuntu: `apt install webp`
+* [Manual Download](https://developers.google.com/speed/webp/download)
 
 ## Usage
 
-``` sh
-converter.sh [-h] [-d DIR] [-o DIR] [-q RATIO] [-r] [-y]
+* Convert images to WebP:
 
-optional arguments:
--h       Show the help message.
--d       Specify the input directory, the default option is the folder where the script is located.
--o       Specify the output directory, if it is empty, the default output path is the same as the original image path.
--q       Quality ratio (0 ~ 100), default is 75.
--r       Process recursively.
--y       Skip confirmation and convert images in the current directory only.
-```
+   ``` Bash
+   img2webp.sh [-h] [-d DIR] [-o DIR] [-q RATIO] [-r] [-y]
+
+   optional arguments:
+   -h       Show the help message.
+   -d       Specify the input directory, the default option is the folder where the script is located.
+   -o       Specify the output directory, if it is empty, the default output path is the same as the original image path.
+   -q       Quality ratio (0 ~ 100), default is 75.
+   -r       Process recursively.
+   -y       Skip confirmation and convert images into WebP in the current directory only.
+   ```
+
+* Convert WebP to PNG:
+
+   ``` Bash
+   webp2png.sh [-h] [-d DIR] [-o DIR] [-r] [-y]
+
+   optional arguments:
+   -h       Show the help message.
+   -d       Specify the input directory, the default option is the folder where the script is located.
+   -o       Specify the output directory, if it is empty, the default output path is the same as the original image path.
+   -r       Process recursively.
+   -y       Skip confirmation and convert WebP images into PNG in the current directory only.
+   ```
 
 ## LICENSE
 
---
+++ webp2png.sh
@@ -0,0 +1,194 @@
+#!/usr/bin/env bash
+#
+# Convert WebP to PNG in batches.
+
+#----------------------------------------------------------
+# fail fast
+set -Eeuo pipefail
+
+# set global var, default value
+if [[ $(uname) == "Darwin" ]]; then
+  OSNAME="MacOS"
+elif [[ $(uname) == "Linux" ]]; then
+  OSNAME=$(cat /etc/*release | grep -E ^ID= | cut -f2 -d"=")
+fi
+readonly OSNAME
+
+INPUT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd -P)  # go and get the location of script
+OUTPUT_DIR=  # null, use the source file location.
+RECURSIVE=false
+
+#----------------------------------------------------------
+# functions
+
+#######################################
+# show help message function ('-h')
+#######################################
+help_message() {
+  cat <<EOF
+A simple conversion script that can bulk convert WebP to PNG.
+----
+usage: webp2png.sh [-h] [-d DIR] [-o DIR] [-r] [-y]
+
+optional arguments:
+-h       Show the help message.
+-d       Specify the input directory, the default option is the folder where the script is located.
+-o       Specify the output directory, if it is empty, the default output path is the same as the original image path.
+-r       Process recursively.
+-y       Skip confirmation and convert WebP images into PNG in the current directory only.
+EOF
+  exit
+}
+
+#######################################
+# error message
+#######################################
+err() {
+  local msg
+  msg="[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*"
+  echo -e "\033[31m$msg\033[0m" >&2  # print in red color
+}
+
+#######################################
+# check file format
+# Globals:
+#   None
+# Arguments:
+#   $1 - file path
+# Returns:
+#   0 - webp
+#   1 - other files
+#######################################
+is_webp() {
+  local suffix
+  suffix=$(echo "${1##*.}" | awk '{print tolower($0)}')
+  if [[ ${suffix} == "webp" ]]; then
+    return 0
+  fi
+  return 1
+}
+
+#######################################
+# recursive function
+#
+# If it is a folder, continue recursively;
+# If it is a picture, convert.
+#
+# Globals:
+#   IFS
+#   RECURSIVE
+#   OUTPUT_DIR
+# Arguments:
+#   $1 - input dir path
+# Returns:
+#   None
+#######################################
+traverse_files() {
+  # change IFS
+  local savedifs=${IFS}
+  IFS=$'\n'
+
+  # check the path ends with "/" or not
+  if [[ ${1:0-1:1} = "/" ]]; then
+    local path="$1*"
+  else
+    local path="$1/*"
+  fi
+
+  # traverse files
+  for file in ${path}; do
+    if [[ -d "${file}" && ${RECURSIVE} == true ]]; then # if it is dir
+      traverse_files "${file}"
+    elif is_webp "${file}" && [[ -f "${file}" && -r "${file}" ]]; then # if it is a webp
+      if [[ -d "${OUTPUT_DIR}" ]]; then # if specify '-o'
+        # extract the image name and rename it to ".png"
+        local filename
+        filename=$(echo "${file##*/}" | cut -f1 -d".")".png"
+        # add output folder prefix
+        if [[ ${OUTPUT_DIR:0-1:1} = "/" ]]; then # check the path ends with "/" or not
+          local output_path="${OUTPUT_DIR}${filename}"
+        else
+          local output_path="${OUTPUT_DIR}/${filename}"
+        fi
+      else
+        local output_path="${file%.*}.png"
+      fi
+
+      # use dwebp to convert
+      dwebp -o "${output_path}" -quiet -mt -- "${file}"  # -mt: multi-thread
+    elif is_webp "${file}" && [[ -f "${file}" && ! -r "${file}" ]]; then # if it is a webp and can not be read
+      err "'${file}' can not be read!"
+    fi
+  done
+
+  # restore IFS
+  IFS=${savedifs}
+}
+
+#----------------------------------------------------------
+# main
+
+# if no input arguments
+if [[ $# -eq 0 ]]; then
+  echo "Execute the conversion (only in the current directory) [Y|N]?"
+  read -rn1 execarg
+  case ${execarg} in
+    Y | y)
+      echo
+      ;;
+    N | n)
+      echo
+      err "Abort."
+      exit 1
+      ;;
+    *)
+      echo
+      err "Unknown input."
+      exit 1
+      ;;
+  esac
+else
+  # get user input argument
+  while getopts "d:ho:ry" opt; do
+    case ${opt} in
+      d)
+        INPUT_DIR=${OPTARG} ;;
+      h)
+        help_message ;; # help function
+      o)
+        OUTPUT_DIR=${OPTARG} ;;
+      r)
+        RECURSIVE=true ;;
+      y)
+        ;;
+      *)
+        err "Unknown option."
+        exit 1 ;;
+    esac
+  done
+fi
+
+# arguments check
+if [[ ! -d "${INPUT_DIR}" ]]; then
+  err "Input directory path[-d]: '${INPUT_DIR}' does not exist!"
+  exit 1
+elif [[ ${OUTPUT_DIR} && ! -d "${OUTPUT_DIR}" ]]; then
+  mkdir "${OUTPUT_DIR}" # create output dir
+fi
+
+# execute conversion
+if type dwebp &> /dev/null; then
+  # dwebp exists
+  traverse_files "${INPUT_DIR}"
+else
+  # dwebp does not exist, install hint
+  err "Sorry, 'dwebp' is not installed in the system."
+  case ${OSNAME} in
+    "MacOS")
+      err "It can be installed through 'brew install webp'." ;;
+    "ubuntu" | "debian")
+      err "It can be installed through 'apt install webp'." ;;
+    *)
+      err "Please download manually from https://developers.google.com/speed/webp/download." ;;
+  esac
+fi
